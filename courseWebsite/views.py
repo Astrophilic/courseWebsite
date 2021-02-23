bd078f66e1e45557913823d9da4942bd116d2e8c
astrophilic <pankajhz99gmail.com>
2021-02-23 21:18:23 +0530

    create templates and views and urls in app and project

--- courseWebsite/views.py
+++ courseWebsite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'courseWebsite/home.html')
+
+
+def cart(request):
+    return render(request, 'courseWebsite/cart.html')
+
+
+def checkout(request):
+    return render(request, 'courseWebsite/checkout.html')
