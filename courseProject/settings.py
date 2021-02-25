3e1f9d83dedb836ebabbcef5269f4520f674b9f2
astrophilic <pankajhz99gmail.com>
2021-02-25 20:10:10 +0530

    adding files

--- courseProject/settings.py
+++ courseProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aim#smtwlwk6z)^kgz$zge0juj9h6jvn6z=9l*3bq@z+y9+i)e'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- courseWebsite/views.py
+++ courseWebsite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -14,4 +15,12 @@ def checkout(request):
 
 
 def register(request):
-    return render(request, 'courseWebsite/register.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'courseWebsite/register.html', context)
